775ed63411f4e11e524b0c2e7b0b27a3935c1d1d
Muhamad Surya Iksanudin <surya@crowde.co>
2021-01-08 20:30:39 +0700

    fix header name

--- handlers/server.go
+++ handlers/server.go
@@ -20,9 +20,9 @@ func NewServer(handler http.Handler) *Server {
 
 func (s *Server) Serve() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		configs.Env.User.ID = r.Header.Get("crowde-user-id")
-		configs.Env.User.Email = r.Header.Get("crowde-user-email")
-		configs.Env.User.Type = r.Header.Get("crowde-user-type")
+		configs.Env.User.ID = r.Header.Get("Crowde-User-Id")
+		configs.Env.User.Email = r.Header.Get("Crowde-User-Email")
+		configs.Env.User.Type = r.Header.Get("Crowde-User-Type")
 
 		s.handler.ServeHTTP(w, r)
 	})
