2121db7c987dff6236a1740634f7f9379c4eb3b2
Muhamad Surya Iksanudin <surya@crowde.co>
2021-01-11 14:38:57 +0700

    add number and word utils

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/crowdeco/skeleton
 go 1.14
 
 require (
+	github.com/dustin/go-humanize v1.0.0
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/golang/protobuf v1.4.3
 	github.com/google/go-cmp v0.5.4 // indirect
---
+++ utils/word.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"regexp"
+	"strings"
+)
+
+type (
+	Word struct {
+	}
+)
+
+func (w *Word) Uppercase(words string) string {
+	return strings.ToUpper(words)
+}
+
+func (w *Word) Lowercase(words string) string {
+	return strings.ToLower(words)
+}
+
+func (w *Word) Underscore(words string) string {
+	words = strings.ToLower(words)
+
+	expr := regexp.MustCompile("[[:space:][:blank:]]")
+	strByte := expr.ReplaceAll([]byte(words), []byte("_"))
+
+	expr = regexp.MustCompile("`[^a-z0-9]`i")
+	strByte = expr.ReplaceAll(strByte, []byte("_"))
+
+	expr = regexp.MustCompile("[!/']")
+	strByte = expr.ReplaceAll(strByte, []byte("_"))
+
+	words = strings.TrimPrefix(string(strByte), "_")
+	words = strings.TrimSuffix(words, "_")
+
+	return words
+}
