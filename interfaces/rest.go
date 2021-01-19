fc0cfab0058174340d47e596cf16527b823e9881
Michael <michael@crowde.co>
2021-01-19 15:17:43 +0700

    add swagger

--- interfaces/rest.go
+++ interfaces/rest.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path"
+	"strings"
 
 	configs "github.com/crowdeco/skeleton/configs"
 	handlers "github.com/crowdeco/skeleton/handlers"
 	middlewares "github.com/crowdeco/skeleton/middlewares"
-	todos "github.com/crowdeco/skeleton/todos"
-
-	runtime "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
+	"google.golang.org/grpc/grpclog"
 )
 
 type (
@@ -23,26 +25,58 @@ func NewRest() configs.Application {
 }
 
 func (g *Rest) Run() {
+	log.Printf("Starting REST Server on :%d", configs.Env.HtppPort)
+
 	ctx := context.Background()
 
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	mux := runtime.NewServeMux()
-	todos.NewServer().RegisterRest(ctx, mux)
+	endpoint := fmt.Sprintf("0.0.0.0:%d", configs.Env.RpcPort)
+	conn, err := grpc.DialContext(ctx, endpoint, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+	defer func() {
+		if err != nil {
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+			return
+		}
+		go func() {
+			<-ctx.Done()
+			if cerr := conn.Close(); cerr != nil {
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+			}
+		}()
+	}()
 
-	err := mux.HandlePath("GET", "/health", func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
-		w.Write([]byte("OK"))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/apidocs/", func(w http.ResponseWriter, r *http.Request) {
+		p := strings.TrimPrefix(r.URL.Path, "/apidocs/")
+		p = path.Join("swagger", p)
+		http.ServeFile(w, r, p)
+	})
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		if s := conn.GetState(); s != connectivity.Ready {
+			http.Error(w, fmt.Sprintf("grpc server is %s", s), http.StatusBadGateway)
+			return
+		}
+		fmt.Fprintln(w, "OK")
 	})
 
+	gw, err := newGateway(ctx, conn)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	mux.Handle("/", gw)
 
 	middleware := handlers.NewMiddleware()
 	middleware.Add(middlewares.NewAuth())
 
-	log.Printf("Starting REST Server on :%d", configs.Env.HtppPort)
+    log.Printf("API Documentation is ready at: http://localhost:%d/apidocs/ui", configs.Env.HtppPort)
 
-	http.ListenAndServe(fmt.Sprintf(":%d", configs.Env.HtppPort), middleware.Attach(mux))
+    http.ListenAndServe(fmt.Sprintf(":%d", configs.Env.HtppPort), middleware.Attach(mux))
 }
--- proto_gen.sh
+++ proto_gen.sh
@@ -1,4 +1,6 @@
-protoc --proto_path=protos --proto_path=libs --go_out=plugins=grpc:protos/builds protos/pagination.proto
+protoc -Iprotos -Ilibs --go_out=plugins=grpc:protos/builds protos/*.proto
+# or
+# protoc --proto_path=protos --proto_path=libs --go_out=plugins=grpc:protos/builds protos/*.proto
 
-protoc --proto_path=protos --proto_path=libs --go_out=plugins=grpc:protos/builds protos/todo.proto
-protoc --proto_path=protos --proto_path=libs --grpc-gateway_out=logtostderr=true:protos/builds protos/todo.proto
+protoc -Iprotos -Ilibs --grpc-gateway_out=logtostderr=true:protos/builds protos/*.proto
+protoc -Iprotos -Ilibs --openapiv2_out=allow_merge=true:swagger protos/*.proto
