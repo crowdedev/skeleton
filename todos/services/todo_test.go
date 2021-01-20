b70cd9e2e1afd355d40734a4c45154fd2a5e3801
Michael <michael@crowde.co>
2021-01-20 10:33:34 +0700

    first test

---
+++ todos/services/todo_test.go
@@ -0,0 +1,52 @@
+package services
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/crowdeco/skeleton/todos/models"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Suite struct {
+	suite.Suite
+	db   *gorm.DB
+	mock sqlmock.Sqlmock
+}
+
+func (s *Suite) SetupSuite() {
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	db, s.mock, err = sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	require.NoError(s.T(), err)
+
+	s.db, err = gorm.Open(mysql.New(mysql.Config{
+		Conn:                      db,
+		SkipInitializeWithVersion: true,
+	}), &gorm.Config{})
+	require.NoError(s.T(), err)
+}
+
+func TestInit(t *testing.T) {
+	suite.Run(t, new(Suite))
+}
+
+func (s *Suite) TestCreate() {
+	s.mock.ExpectExec("INSERT INTO `todos` (`name`) VALUES (?)").
+		WithArgs("Todo 1").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	service := NewTodoService(s.db)
+	service.Create(&models.Todo{Name: "Todo 1"})
+
+	if err := s.mock.ExpectationsWereMet(); err != nil {
+		s.T().Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
