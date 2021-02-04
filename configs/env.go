2bbad41b4882be96c730f739a3490559d9d5362d
Muhamad Surya Iksanudin <surya@crowde.co>
2021-02-05 01:06:00 +0700

    improve class

--- configs/env.go
+++ configs/env.go
@@ -1,33 +1,31 @@
 package configs
 
-type (
-	Env struct {
-		Debug              bool
-		HtppPort           int
-		RpcPort            int
-		Version            string
-		ServiceName        string
-		DbHost             string
-		DbPort             int
-		DbUser             string
-		DbPassword         string
-		DbName             string
-		DbDriver           string
-		DbAutoMigrate      bool
-		ElasticsearchHost  string
-		ElasticsearchPort  int
-		ElasticsearchIndex string
-		MongoDbHost        string
-		MongoDbPort        int
-		MongoDbName        string
-		AmqpHost           string
-		AmqpPort           int
-		AmqpUser           string
-		AmqpPassword       string
-		HeaderUserId       string
-		HeaderUserEmail    string
-		HeaderUserRole     string
-		CacheLifetime      int
-		User               *User
-	}
-)
+type Env struct {
+	Debug              bool
+	HtppPort           int
+	RpcPort            int
+	Version            string
+	ServiceName        string
+	DbHost             string
+	DbPort             int
+	DbUser             string
+	DbPassword         string
+	DbName             string
+	DbDriver           string
+	DbAutoMigrate      bool
+	ElasticsearchHost  string
+	ElasticsearchPort  int
+	ElasticsearchIndex string
+	MongoDbHost        string
+	MongoDbPort        int
+	MongoDbName        string
+	AmqpHost           string
+	AmqpPort           int
+	AmqpUser           string
+	AmqpPassword       string
+	HeaderUserId       string
+	HeaderUserEmail    string
+	HeaderUserRole     string
+	CacheLifetime      int
+	User               *User
+}
