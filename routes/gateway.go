e3d85d796544811a0c63bd968c4d79279edc3a54
Muhamad Surya Iksanudin <70564773+surya4crowde@users.noreply.github.com>
2021-02-07 07:17:51 +0700

    Fix override server

--- routes/gateway.go
+++ routes/gateway.go
@@ -16,8 +16,8 @@ type GRpcGateway struct {
 func (g *GRpcGateway) Handle(ctx context.Context, server *http.ServeMux, client *grpc.ClientConn) *http.ServeMux {
 	mux := runtime.NewServeMux()
 
-	for _, server := range g.Servers {
-		server.GRpcHandler(ctx, mux, client)
+	for _, handler := range g.Servers {
+		handler.GRpcHandler(ctx, mux, client)
 	}
 
 	server.Handle("/", mux)
