980ce96927dc0d9a6f9583cb1d770757593dbed8
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2021-02-10 10:32:32 +0700

    fixing module auto register

--- generators/dic.go
+++ generators/dic.go
@@ -13,10 +13,7 @@ type Dic struct {
 
 func (g *Dic) Generate(template *configs.Template, modulePath string, workDir string, templatePath string) {
 	dicTemplate, _ := engine.ParseFiles(fmt.Sprintf("%s/%s/dic.tpl", workDir, templatePath))
-	dicPath := fmt.Sprintf("%s/dics/modules", workDir)
-	os.MkdirAll(dicPath, 0755)
-
-	dicFile, err := os.Create(fmt.Sprintf("%s/%s.go", dicPath, template.ModuleLowercase))
+	dicFile, err := os.Create(fmt.Sprintf("%s/%s.go", modulePath, template.ModuleLowercase))
 	if err != nil {
 		panic(err)
 	}
