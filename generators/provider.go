405c341f76a9da475072109cd49de1e3b6af5266
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2021-02-10 10:55:50 +0700

    fixing module auto register

--- generators/provider.go
+++ generators/provider.go
@@ -38,7 +38,7 @@ func (p *Provider) Generate(template *configs.Template, modulePath string, workD
 
 	if !skipImport {
 		contents[importIdx] = fmt.Sprintf(`    //%s
-    %s %q`, MODULE_IMPORT, template.ModuleLowercase, fmt.Sprintf("%s/%s", template.PackageName, template.ModulePluralLowercase))
+    %s %q`, MODULE_IMPORT, template.ModuleLowercase, fmt.Sprintf("%s/%s/dics", template.PackageName, template.ModulePluralLowercase))
 	}
 
 	contents[moduleIdx] = fmt.Sprintf(`
