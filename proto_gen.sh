0c66810ef683a1159ac297d618ffad3afd33b743
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2021-02-16 20:14:16 +0700

    improve swagger

--- proto_gen.sh
+++ proto_gen.sh
@@ -2,4 +2,4 @@ protoc -Iprotos -Ilibs --go_out=plugins=grpc:protos/builds protos/*.proto
 protoc -Iprotos -Ilibs --grpc-gateway_out=logtostderr=true:protos/builds protos/*.proto
 protoc -Iprotos -Ilibs --go_out=plugins=grpc:protos/builds libs/bima/*.proto
 protoc -Iprotos -Ilibs --grpc-gateway_out=logtostderr=true:protos/builds libs/bima/*.proto
-protoc -Iprotos -Ilibs --openapiv2_out=allow_merge=true:swaggers protos/*.proto
+protoc -Iprotos -Ilibs --openapiv2_out=swaggers protos/*.proto
